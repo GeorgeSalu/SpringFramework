60c9ac7e52411ca3bddbbff63a406207f8bf0a20
George salu <george.salu10@gmail.com>
2015-08-11 15:15:19 -0300

    configuração do mvc do spring e do web.xml programaticamente, inclusão do jstl no pom.xml do maven

---
+++ app-final/src/main/java/br/com/appfinal/config/MvcConfig.java
@@ -0,0 +1,26 @@
+package br.com.appfinal.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan("br.com.appfinal")
+public class MvcConfig {
+
+	@Bean(name="jspViewResolver")
+	public InternalResourceViewResolver internalResourceViewResolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		
+		resolver.setPrefix("/WEB-INF/views");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		
+		return resolver;
+	}
+	
+}
---
+++ app-final/src/main/java/br/com/appfinal/config/WebXmlConfig.java
@@ -0,0 +1,30 @@
+package br.com.appfinal.config;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class WebXmlConfig implements WebApplicationInitializer{
+
+	@Override
+	public void onStartup(ServletContext servletContext) throws ServletException {
+		
+		AnnotationConfigWebApplicationContext webContext = 
+				new AnnotationConfigWebApplicationContext();
+		
+		webContext.register(MvcConfig.class);
+		webContext.setServletContext(servletContext);
+		
+		ServletRegistration.Dynamic reDynamic = 
+				servletContext.addServlet("dispacher", new DispatcherServlet());
+		
+		reDynamic.setLoadOnStartup(1);
+		reDynamic.addMapping("/");
+		
+	}
+
+}
