6a4e30eb3c81ee24690bfd3c1f20e8f8e7d76ca2
George salu <george.salu10@gmail.com>
2015-08-19 19:01:28 -0300

    implementação de EnderecoDao e EnderecoService

---
+++ app-final/src/main/java/br/com/appfinal/service/EnderecoService.java
@@ -0,0 +1,37 @@
+package br.com.appfinal.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.appfinal.dao.EnderecoDao;
+import br.com.appfinal.entity.Endereco;
+
+@Service
+public class EnderecoService {
+
+	@Autowired
+	private EnderecoDao enderecoDao;
+	
+	public Endereco save(Endereco endereco){
+		if(endereco.getIdEndereco() == null){
+			endereco = enderecoDao.save(endereco);
+		}else{
+			enderecoDao.update(endereco);
+		}
+		return endereco;
+	}
+	
+	public void delete(Integer id){
+		enderecoDao.delete(id);
+	}
+	
+	public Endereco findById(Integer id){
+		return enderecoDao.findById(id);
+	}
+	
+	public List<Endereco> findAll(){
+		return enderecoDao.findAll();
+	}
+}
