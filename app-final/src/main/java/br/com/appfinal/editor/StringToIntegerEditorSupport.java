d817c22836b017d2502279206ee2e2c67eef9b4a
George salu <george.salu10@gmail.com>
2015-08-23 22:48:19 -0300

    conversor StringToInteger

--- app-final/src/main/java/br/com/appfinal/editor/StringToIntegerEditorSupport.java
+++ app-final/src/main/java/br/com/appfinal/editor/StringToIntegerEditorSupport.java
@@ -1,5 +1,22 @@
 package br.com.appfinal.editor;
 
-public class StringToIntegerEditorSupport {
+import java.beans.PropertyEditorSupport;
 
+import org.apache.log4j.Logger;
+
+public class StringToIntegerEditorSupport extends PropertyEditorSupport{
+
+	private static Logger logger = Logger.getLogger(StringToIntegerEditorSupport.class);
+	
+	@Override
+	public void setAsText(String text) {
+		
+		try {
+			super.setValue(Integer.parseInt(text));
+		} catch (NumberFormatException ex) {
+			logger.fatal("O campo numero esperava um inteiro e recebeu uma string ",ex);
+		}
+		
+	}
+	
 }
